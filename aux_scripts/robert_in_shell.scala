3965d99ed009d3ffa56dd661f6b7a05793809d3b
robertmacdavid <robertmacdavid@gmail.com>
2017-05-14 13:11:08 -0400

    batch script update

--- aux_scripts/robert_in_shell.scala
+++ aux_scripts/robert_in_shell.scala
@@ -1,13 +1,16 @@
 
-val batch_count = 10
-val partition_count = 100
-val input_filename = "data/students.json"
+val batch_count = 100
+val partition_count = 400
+val query = "SELECT AVG(uniform) FROM students"
 
+
+val input_filename = "data/students.json"
+val output_filename = "students_whoaaaa.dat"
+
+import java.io._
 import org.apache.spark.sql.types._
 import org.apache.spark.sql.Row
 import org.apache.spark.HashPartitioner
-
-
 import sqlContext.implicits._
 import org.apache.spark.sql.hive.online.OnlineSQLConf._
 import org.apache.spark.sql.hive.online.OnlineSQLFunctions._
@@ -15,40 +18,26 @@ import org.apache.spark.sql.hive.online.OnlineSQLFunctions._
 
 sqlContext.setConf(STREAMED_RELATIONS, "students")
 sqlContext.setConf(NUMBER_BATCHES, batch_count.toString)
-/*
-// val schema = new StructType(Array( 
-//                StructField("name", StringType, false),
-//                StructField("index", LongType, false),
-                StructField("coin_toss", IntegerType, false),
-                StructField("normal", DoubleType, false),
-                StructField("zipf", LongType, false),
-                StructField("uniform", IntegerType, false),
-                StructField("twin_peak", DoubleType, false),
-                StructField("geometric", IntegerType, false),
-                StructField("exponential", LongType, false),
-                StructField("lognormal", DoubleType, false),
-                StructField("zero", IntegerType, false)))
-*/
 
-//var rdd = sc.textFile("aux_scripts/interesting_data.csv").map(line=>Row.fromSeq(line.split(",")))
-var df = sqlContext.read
-    .format("com.databricks.spark.csv")
-    .option("header", "true") // Use first line of all files as header
-    .option("inferSchema", "true") // Automatically infer data types
-    .load("aux_scripts/interesting_data.csv.csv")
+var df = sqlContext.read.format("com.databricks.spark.csv").option("header", "true").option("inferSchema", "true").load("aux_scripts/interesting_data.csv")
+
+df = df.repartition(partition_count)
 
 val schema = df.schema
 val rdd = df.rdd.repartition(partition_count)
 val df = sqlContext.createDataFrame(rdd, schema)
-//rdd = rdd.repartition(partition_count)
 
-//val df = sqlContext.createDataFrame(rdd,schema)
-df.show()
-//df.registerTempTable("students")
-//val odf = sqlContext.sql("SELECT AVG(uniform) FROM students").online
+//df.show()
+df.registerTempTable("students")
+val odf = sqlContext.sql(query).online
 
-//var results : Array[Array[String]] = new Array[Array[String]](10)
-//for (i <- 0 to batch_count-1) {
-//	val subresult = odf.collectNext() // Should be an array of 3 values: [Approx Ans, Lower Conf, Upper Conf]
-//	results(i) = subresult.map(x -> x.toString)
-//}
+val pw = new PrintWriter(new File(output_filename))
+{
+        odf.hasNext
+        val result = (1 to odf.progress._2).map { i => assert(odf.hasNext); odf.collectNext() }
+        result.map { r => 
+          (r(0).get(0).asInstanceOf[org.apache.spark.sql.Row].getDouble(0),
+          r(0).get(0).asInstanceOf[org.apache.spark.sql.Row].getDouble(1),
+          r(0).get(0).asInstanceOf[org.apache.spark.sql.Row].getDouble(2)) }.zipWithIndex.foreach { case ((a, b, c), i) => pw.println(s"${i+1} $a $b $c") }
+}
+pw.close()

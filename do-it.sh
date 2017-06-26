1519676455c8a448d4a7eecc5ffc6229e4b9c894
Andrew Or <andrewor14@gmail.com>
2017-06-26 15:40:41 -0400

    Add handy dandy script to run many experiments

---
+++ do-it.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+INPUT_PATH="data/students.json" ./run-iolap.sh true 10 100
+INPUT_PATH="data/students.json" ./run-iolap.sh false 10 100
+INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 100
+INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 100
+INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 100
+INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 100
+INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 100
+INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 100
+
+INPUT_PATH="data/students.json" ./run-iolap.sh true 10 500
+INPUT_PATH="data/students.json" ./run-iolap.sh false 10 500
+INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 500
+INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 500
+INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
+INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
+INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 500
+INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 500
+
+NUM_PARTS="500" INPUT_PATH="data/students.json" ./run-iolap.sh true 10 500
+NUM_PARTS="500" INPUT_PATH="data/students.json" ./run-iolap.sh false 10 500
+NUM_PARTS="500" INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 500
+NUM_PARTS="500" INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 500
+NUM_PARTS="500" INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
+NUM_PARTS="500" INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
+NUM_PARTS="500" INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 500
+NUM_PARTS="500" INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 500
+
+INPUT_PATH="data/students.json" ./run-iolap.sh true 10 1000
+INPUT_PATH="data/students.json" ./run-iolap.sh false 10 1000
+INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 1000
+INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 1000
+INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 1000
+INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 1000
+INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 1000
+INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 1000
+

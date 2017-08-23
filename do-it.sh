1cd154d1ea12594581e4156f4babe1abc3551b03
Andrew Or <andrewor14@gmail.com>
2017-08-23 14:11:56 -0400

    Experiment: align the experiments?

--- do-it.sh
+++ do-it.sh
@@ -9,12 +9,14 @@
 #INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 100
 #INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 100
 
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students.json" ./run-iolap.sh true 10 500
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students.json" ./run-iolap.sh false 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students.json" ./run-iolap.sh true 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students.json" ./run-iolap.sh false 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 500
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh true 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh false 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 500
 
@@ -36,5 +38,5 @@
 #INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 1000
 #INPUT_PATH="data/students10g.json" ./run-iolap.sh false 10 1000
 
-PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh true 1 500
+#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh true 1 500
 

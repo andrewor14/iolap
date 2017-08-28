925b4a29f1c7f85e31913975f6f9cf2cc76fe4ed
Andrew Or <andrewor14@gmail.com>
2017-08-28 13:01:22 -0400

    Experiment after simplifying: 1g + 5g

--- do-it.sh
+++ do-it.sh
@@ -13,8 +13,8 @@
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students.json" ./run-iolap.sh false 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10.json" ./run-iolap.sh true 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10.json" ./run-iolap.sh false 10 500
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
-#PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh true 10 500
+PREPARE_DATAFRAMES="true" INPUT_PATH="data/students1g.json" ./run-iolap.sh false 10 500
 PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh true 10 500
 PREPARE_DATAFRAMES="true" INPUT_PATH="data/students5g.json" ./run-iolap.sh false 10 500
 #PREPARE_DATAFRAMES="true" INPUT_PATH="data/students10g.json" ./run-iolap.sh true 10 500

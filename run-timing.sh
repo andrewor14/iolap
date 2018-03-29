3b745891f76e4dd11d68bd6e956c32dbfd203bb5
Andrew Or <andrewor14@gmail.com>
2018-03-29 17:23:23 -0400

    Make timing run script more customizable

--- run-timing.sh
+++ run-timing.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
-LOG_DIR="/disk/local/disk2/andrew/logs"
-INPUT_DATA="/disk/local/disk2/andrew/data/students1g.json"
+NUM_BATCHES="${NUM_BATCHES:-100}"
+
+LOG_DIR="${LOG_DIR:-/disk/local/disk2/andrew/logs}"
+INPUT_DATA="${INPUT_DATA:-/disk/local/disk2/andrew/data/students1g.json}"
+SHOULD_CACHE_TABLES="${SHOULD_CACHE_TABLES:-true}"
+EXPR_NAME="${EXPR_NAME:-timing}"
 
 bin/spark-submit\
   --master local[*]\
-  --driver-memory 40g\
-  --executor-memory 40g\
+  --driver-memory 45g\
+  --executor-memory 45g\
   --class org.apache.spark.examples.sql.hive.TestIolapPR\
+  --conf spark.app.name="$EXPR_NAME"\
   --conf spark.approx.logDir="$LOG_DIR"\
   --conf spark.approx.inputFiles="$INPUT_DATA"\
+  --conf spark.approx.shouldCacheTables="$SHOULD_CACHE_TABLES"\
   --conf spark.eventLog.enabled="true"\
   --conf spark.eventLog.dir="$LOG_DIR"\
-  examples/target/scala-2.10/spark-examples-1.4.3-SNAPSHOT-hadoop2.2.0.jar 2>&1 | tee "$LOG_DIR/"timing.log
+  examples/target/scala-2.10/spark-examples-1.4.3-SNAPSHOT-hadoop2.2.0.jar 2>&1 | tee "$LOG_DIR/$EXPR_NAME".log
 

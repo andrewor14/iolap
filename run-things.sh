88538921ca6ab1ff24d1b7a7e30be9618f9ead4d
Andrew Or <andrewor14@gmail.com>
2018-04-10 14:17:04 -0400

    Fix no cache behavior (again)

--- run-things.sh
+++ run-things.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 INPUT_PATH="/disk/local/disk2/andrew/data/"
-INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_DATA=true EXPR_NAME="timing_1g_cache" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_DATA=false EXPR_NAME="timing_1g_nocache" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_DATA=true EXPR_NAME="timing_30g_cache" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_DATA=false EXPR_NAME="timing_30g_nocache" ./run-timing.sh
+INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_TABLES=true EXPR_NAME="timing_1g_cache" ./run-timing.sh
+INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_TABLES=false EXPR_NAME="timing_1g_nocache" ./run-timing.sh
+INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=true EXPR_NAME="timing_30g_cache" ./run-timing.sh
+INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false EXPR_NAME="timing_30g_nocache" ./run-timing.sh
 

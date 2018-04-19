b2a02e712181c2d740cd468f8f2ad1a4fee2b546
Andrew Or <andrewor14@gmail.com>
2018-04-19 14:12:44 -0400

    Copy over names of configs to run-things

--- run-things.sh
+++ run-things.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Available runtime configs:
+#   - LOG_DIR
+#   - INPUT_DATA
+#   - NUM_BATCHES
+#   - NUM_PARTITIONS
+#   - NUM_BOOTSTRAP_TRIALS
+#   - SHOULD_CACHE_TABLES
+#   - IOLAP_CACHE_ENABLED
+#   - EXPR_NAME
+#   - RUNTIME_MEMORY
+
 INPUT_PATH="/disk/local/disk2/andrew/data/"
 #INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_TABLES=true EXPR_NAME="timing_1g_cache" ./run-timing.sh
 #INPUT_DATA="$INPUT_PATH/students1g.json" SHOULD_CACHE_TABLES=false EXPR_NAME="timing_1g_nocache" ./run-timing.sh
@@ -7,8 +18,8 @@ INPUT_PATH="/disk/local/disk2/andrew/data/"
 #INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false EXPR_NAME="timing_30g_nocache" ./run-timing.sh
 
 RUNTIME_MEMORY="20g"
-INPUT_DATA="$INPUT_PATH/students12g.json" SHOULD_CACHE_TABLES=true IOLAP_CACHE_ENABLED=true EXPR_NAME="12g_cache" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=true IOLAP_CACHE_ENABLED=true EXPR_NAME="30g_cache" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false IOLAP_CACHE_ENABLED=true EXPR_NAME="30g_iolap_cache_only" ./run-timing.sh
-INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false IOLAP_CACHE_ENABLED=false EXPR_NAME="30g_no_cache" ./run-timing.sh
+#INPUT_DATA="$INPUT_PATH/students12g.json" SHOULD_CACHE_TABLES=true IOLAP_CACHE_ENABLED=true EXPR_NAME="12g_cache" ./run-timing.sh
+#INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=true IOLAP_CACHE_ENABLED=true EXPR_NAME="30g_cache" ./run-timing.sh
+#INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false IOLAP_CACHE_ENABLED=true EXPR_NAME="30g_iolap_cache_only" ./run-timing.sh
+#INPUT_DATA="$INPUT_PATH/students30g.json" SHOULD_CACHE_TABLES=false IOLAP_CACHE_ENABLED=false EXPR_NAME="30g_no_cache" ./run-timing.sh
 

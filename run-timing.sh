3c70e7b92fdb8d05742a4163a86a8e3febee2895
Andrew Or <andrewor14@gmail.com>
2018-04-20 15:08:33 -0400

    Print experiment configs in run-timing.sh

--- run-timing.sh
+++ run-timing.sh
@@ -12,6 +12,22 @@ IOLAP_CACHE_ENABLED="${IOLAP_CACHE_ENABLED:-true}"
 EXPR_NAME="${EXPR_NAME:-timing}"
 RUNTIME_MEMORY="${RUNTIME_MEMORY:-45g}"
 
+# Print all flags to log file
+LOG_FILE_NAME="$LOG_DIR/$EXPR_NAME".log
+echo "===========================================================================
+  LOG_DIR=$LOG_DIR
+  INPUT_DATA=$INPUT_DATA
+  NUM_BATCHES=$NUM_BATCHES
+  NUM_PARTITIONS=$NUM_PARTITIONS
+  NUM_BOOTSTRAP_TRIALS=$NUM_BOOTSTRAP_TRIALS
+  SHOULD_CACHE_TABLES=$SHOULD_CACHE_TABLES
+  IOLAP_CACHE_ENABLED=$IOLAP_CACHE_ENABLED
+  EXPR_NAME=$EXPR_NAME
+  RUNTIME_MEMORY=$RUNTIME_MEMORY
+===========================================================================
+" > "$LOG_FILE_NAME"
+
+# Run Spark
 bin/spark-submit\
   --master local[*]\
   --driver-memory "$RUNTIME_MEMORY"\
@@ -27,5 +43,5 @@ bin/spark-submit\
   --conf spark.approx.iolapCacheEnabled="$IOLAP_CACHE_ENABLED"\
   --conf spark.eventLog.enabled="true"\
   --conf spark.eventLog.dir="$LOG_DIR"\
-  examples/target/scala-2.10/spark-examples-1.4.3-SNAPSHOT-hadoop2.2.0.jar > "$LOG_DIR/$EXPR_NAME".log 2>&1
+  examples/target/scala-2.10/spark-examples-1.4.3-SNAPSHOT-hadoop2.2.0.jar >> "$LOG_FILE_NAME" 2>&1
 
